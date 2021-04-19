e98805487e4cfca049c69806c885416cd399a465
Sofia K <sofiakovalevskaia@gmail.com>
2021-04-19 14:57:51 +0100

    setup express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.status(200).json({"message": "ok"});
+});
+
+app.listen(5000, ()=>{
+    console.log("App is online");
+});
